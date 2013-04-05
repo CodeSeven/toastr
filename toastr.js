e15782771ffdc1a7ae6736a0f1676d7f47e74594
John Papa <john@johnpapa.net>
2013-04-05 10:33:29 -0300

    Update toastr.js

--- toastr.js
+++ toastr.js
@@ -30,7 +30,7 @@
                     },
                     iconClass: 'toast-info',
                     positionClass: 'toast-top-right',
-                    timeOut: 5000, // Set timeOut to 0 to make it sticky
+                    timeOut: 5000, // Set timeOut and extendedTimeout to 0 to make it sticky
                     titleClass: 'toast-title',
                     messageClass: 'toast-message',
                     target: 'body'
