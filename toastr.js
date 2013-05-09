f0d77ece7717cd31923e0966f4b5b32a76c62d7e
John Papa <john@johnpapa.net>
2013-05-09 18:55:20 -0400

    updated demo with 1.3.0

--- toastr.js
+++ toastr.js
@@ -10,7 +10,7 @@
 ; (function (define) {
     define(['jquery'], function ($) {
         return (function () {
-            var version = '1.2.3',
+            var version = '1.3.0',
                 $container,
 
                 defaults = {
