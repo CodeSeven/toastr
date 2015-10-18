d1aad2077cc5bdcaf1e1712399cdf2cb841b10ef
Andreas Taube <andreas.taube@codecentric.de>
2015-10-19 00:57:51 +0200

    clear delayed hide toast handlers if a toast has been hidden in the meanwhile

--- toastr.js
+++ toastr.js
@@ -375,6 +375,7 @@
                         easing: easing,
                         complete: function () {
                             removeToast($toastElement);
+                            clearTimeout(intervalId);
                             if (options.onHidden && response.state !== 'hidden') {
                                 options.onHidden();
                             }
