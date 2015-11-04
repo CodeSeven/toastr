d3e01ead931d9292a14c16b02f0412db74c3ea77
Victor Carreno <vcarreno@dssinc.com>
2015-11-04 12:07:35 -0500

    fixed issue with screen reader duplicate announcements

--- toastr.js
+++ toastr.js
@@ -145,7 +145,6 @@
                 $container = $('<div/>')
                     .attr('id', options.containerId)
                     .addClass(options.positionClass)
-                    .attr('aria-live', 'polite')
                     .attr('role', 'alert');
 
                 $container.appendTo($(options.target));
