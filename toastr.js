50ff88599a401479364cdfb35ff55666331c99e7
Timothy Ferrell <timothyferrell@gmail.com>
2016-07-07 13:30:49 -0400

    Removing role=alert to resolve #395.

--- toastr.js
+++ toastr.js
@@ -145,7 +145,6 @@
                 $container = $('<div/>')
                     .attr('id', options.containerId)
                     .addClass(options.positionClass)
-                    .attr('role', 'alert');
 
                 $container.appendTo($(options.target));
                 return $container;
