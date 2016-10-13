ec2d6852a100f2d71cb1424da71186e6c39693df
Quezler <Quezler@me.com>
2016-10-13 10:05:31 +0200

    Add onCreate callback
    onCreate will run when before the toast is faded in,
    onShown runs when it is fully faded in.

--- toastr.js
+++ toastr.js
@@ -157,6 +157,7 @@
                     containerId: 'toast-container',
                     debug: false,
 
+                    onCreate: undefined,
                     showMethod: 'fadeIn', //fadeIn, slideDown, and show are built into jQuery
                     showDuration: 300,
                     showEasing: 'swing', //swing and linear are built into jQuery
@@ -319,6 +320,8 @@
 
                 function displayToast() {
                     $toastElement.hide();
+                    
+                    options.onCreate();
 
                     $toastElement[options.showMethod](
                         {duration: options.showDuration, easing: options.showEasing, complete: options.onShown}
@@ -468,7 +471,7 @@
         })();
     });
 }(typeof define === 'function' && define.amd ? define : function (deps, factory) {
-    if (typeof module !== 'undefined' && module.exports && typeof require === 'function') { //Node
+    if (typeof module !== 'undefined' && module.exports) { //Node
         module.exports = factory(require('jquery'));
     } else {
         window.toastr = factory(window.jQuery);
