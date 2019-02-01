5ca4b4d47d0c3892771ed46748508c25c55eab56
JPeer264 <jan.oster94@gmail.com>
2019-02-01 17:42:00 +0100

    Chore: remove rollup docstrings, add dynamic style build

--- rollup.config.ts
+++ rollup.config.ts
@@ -22,35 +22,26 @@ export default {
       sourcemap: true,
     },
   ],
-  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
   external: ['lodash'],
   watch: {
     include: 'src/**',
   },
   plugins: [
-    // Allow json resolution
     json(),
-    // Compile TypeScript files
     typescript({
       useTsconfigDeclarationDir: true,
     }),
-    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs({
       include: 'node_modules/**',
     }),
-    // Allow node_modules resolution, so you can use 'external' to control
-    // which external modules to include in the bundle
-    // https://github.com/rollup/rollup-plugin-node-resolve#usage
     resolve({
       browser: true,
     }),
-
-    // Resolve source maps to the original source
     sourceMaps(),
 
     postcss({
       sourceMap: true,
-      extract: 'dist/toastr.min.css',
+      extract: pkg.style,
       minimize: true,
     }),
   ],
