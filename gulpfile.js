cbe9ec12d6a7c1cf59e8093517dad8cc4aa11394
John Papa <john@johnpapa.net>
2019-05-23 09:15:50 -0400

    npm audit fix and gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -5,76 +5,74 @@ var merge = require('merge-stream');
 var plug = require('gulp-load-plugins')();
 
 var paths = {
-    js: './toastr.js',
-    less: './toastr.less',
-    report: './report',
-    build: './build'
+  js: './toastr.js',
+  less: './toastr.less',
+  report: './report',
+  build: './build'
 };
 
 var log = plug.util.log;
 
 /**
- * List the available gulp tasks
- */
-gulp.task('help', plug.taskListing);
-
-/**
  * Lint the code, create coverage report, and a visualizer
  * @return {Stream}
  */
-gulp.task('analyze', function () {
-    log('Analyzing source with JSHint and JSCS');
+gulp.task('analyze', function() {
+  log('Analyzing source with JSHint and JSCS');
 
-    var jshint = analyzejshint([paths.js]);
-    var jscs = analyzejscs([paths.js]);
+  var jshint = analyzejshint([paths.js]);
+  var jscs = analyzejscs([paths.js]);
 
-    return merge(jshint, jscs);
+  return merge(jshint, jscs);
 });
 
 /**
  * Minify and bundle the app's JavaScript
  * @return {Stream}
  */
-gulp.task('js', function () {
-    log('Bundling, minifying, and copying the app\'s JavaScript');
+gulp.task('js', function() {
+  log("Bundling, minifying, and copying the app's JavaScript");
 
-    return gulp
-        .src(paths.js)
-        .pipe(plug.sourcemaps.init())
-        .pipe(plug.bytediff.start())
-        .pipe(plug.uglify({}))
-        .pipe(plug.bytediff.stop(bytediffFormatter))
-        .pipe(plug.sourcemaps.write('.'))
-        .pipe(plug.rename(function (path) {
-            if (path.extname === '.js') {
-                path.basename += '.min';
-            }
-        }))
-        .pipe(gulp.dest(paths.build));
+  return gulp
+    .src(paths.js)
+    .pipe(plug.sourcemaps.init())
+    .pipe(plug.bytediff.start())
+    .pipe(plug.uglify({}))
+    .pipe(plug.bytediff.stop(bytediffFormatter))
+    .pipe(plug.sourcemaps.write('.'))
+    .pipe(
+      plug.rename(function(path) {
+        if (path.extname === '.js') {
+          path.basename += '.min';
+        }
+      })
+    )
+    .pipe(gulp.dest(paths.build));
 });
 
 /**
  * Minify and bundle the CSS
  * @return {Stream}
  */
-gulp.task('css', function () {
-    log('Bundling, minifying, and copying the app\'s CSS');
+gulp.task('css', function() {
+  log("Bundling, minifying, and copying the app's CSS");
 
-    return gulp.src(paths.less)
-        .pipe(plug.less())
-        .pipe(gulp.dest(paths.build))
-        .pipe(plug.bytediff.start())
-        .pipe(plug.minifyCss({}))
-        .pipe(plug.bytediff.stop(bytediffFormatter))
-        .pipe(plug.rename('toastr.min.css'))
-        .pipe(gulp.dest(paths.build));
+  return gulp
+    .src(paths.less)
+    .pipe(plug.less())
+    .pipe(gulp.dest(paths.build))
+    .pipe(plug.bytediff.start())
+    .pipe(plug.minifyCss({}))
+    .pipe(plug.bytediff.stop(bytediffFormatter))
+    .pipe(plug.rename('toastr.min.css'))
+    .pipe(gulp.dest(paths.build));
 });
 
 /**
  * Build js and css
  */
-gulp.task('default', ['js', 'css'], function () {
-    log('Analyze, Build CSS and JS');
+gulp.task('default', gulp.parallel('js', 'css'), function() {
+  log('Analyze, Build CSS and JS');
 });
 
 /**
@@ -83,12 +81,12 @@ gulp.task('default', ['js', 'css'], function () {
  * from the cmd line: gulp clean && gulp build
  * @return {Stream}
  */
-gulp.task('clean', function (cb) {
-    log('Cleaning: ' + plug.util.colors.blue(paths.report));
-    log('Cleaning: ' + plug.util.colors.blue(paths.build));
+gulp.task('clean', function(cb) {
+  log('Cleaning: ' + plug.util.colors.blue(paths.report));
+  log('Cleaning: ' + plug.util.colors.blue(paths.build));
 
-    var delPaths = [paths.build, paths.report];
-    del(delPaths, cb);
+  var delPaths = [paths.build, paths.report];
+  del(delPaths, cb);
 });
 
 /**
@@ -97,8 +95,8 @@ gulp.task('clean', function (cb) {
  *    gulp test --startServers
  * @return {Stream}
  */
-gulp.task('test', function (done) {
-    startTests(true /*singleRun*/, done);
+gulp.task('test', function(done) {
+  startTests(true /*singleRun*/, done);
 });
 
 ////////////////
@@ -110,12 +108,12 @@ gulp.task('test', function (done) {
  * @return {Stream}
  */
 function analyzejshint(sources, overrideRcFile) {
-    var jshintrcFile = overrideRcFile || './.jshintrc';
-    log('Running JSHint');
-    return gulp
-        .src(sources)
-        .pipe(plug.jshint(jshintrcFile))
-        .pipe(plug.jshint.reporter('jshint-stylish'));
+  var jshintrcFile = overrideRcFile || './.jshintrc';
+  log('Running JSHint');
+  return gulp
+    .src(sources)
+    .pipe(plug.jshint(jshintrcFile))
+    .pipe(plug.jshint.reporter('jshint-stylish'));
 }
 
 /**
@@ -124,10 +122,8 @@ function analyzejshint(sources, overrideRcFile) {
  * @return {Stream}
  */
 function analyzejscs(sources) {
-    log('Running JSCS');
-    return gulp
-        .src(sources)
-        .pipe(plug.jscs('./.jscsrc'));
+  log('Running JSCS');
+  return gulp.src(sources).pipe(plug.jscs('./.jscsrc'));
 }
 
 /**
@@ -137,20 +133,23 @@ function analyzejscs(sources) {
  * @return {undefined}
  */
 function startTests(singleRun, done) {
-    karma.start({
-        configFile: __dirname + '/karma.conf.js',
-        singleRun: !!singleRun
-    }, karmaCompleted);
+  karma.start(
+    {
+      configFile: __dirname + '/karma.conf.js',
+      singleRun: !!singleRun
+    },
+    karmaCompleted
+  );
 
-    ////////////////
+  ////////////////
 
-    function karmaCompleted(exitCode) {
-        if (exitCode === 0) {
-            done();
-        } else {
-            process.exit(exitCode);
-        }
+  function karmaCompleted(exitCode) {
+    if (exitCode === 0) {
+      done();
+    } else {
+      process.exit(exitCode);
     }
+  }
 }
 
 /**
@@ -159,10 +158,19 @@ function startTests(singleRun, done) {
  * @return {String}      Difference in bytes, formatted
  */
 function bytediffFormatter(data) {
-    var difference = (data.savings > 0) ? ' smaller.' : ' larger.';
-    return data.fileName + ' went from ' +
-        (data.startSize / 1000).toFixed(2) + ' kB to ' + (data.endSize / 1000).toFixed(2) + ' kB' +
-        ' and is ' + formatPercent(1 - data.percent, 2) + '%' + difference;
+  var difference = data.savings > 0 ? ' smaller.' : ' larger.';
+  return (
+    data.fileName +
+    ' went from ' +
+    (data.startSize / 1000).toFixed(2) +
+    ' kB to ' +
+    (data.endSize / 1000).toFixed(2) +
+    ' kB' +
+    ' and is ' +
+    formatPercent(1 - data.percent, 2) +
+    '%' +
+    difference
+  );
 }
 
 /**
@@ -172,5 +180,5 @@ function bytediffFormatter(data) {
  * @return {Number}           Formatted perentage
  */
 function formatPercent(num, precision) {
-    return (num * 100).toFixed(precision);
+  return (num * 100).toFixed(precision);
 }
