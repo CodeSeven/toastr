4a2d85cae6ed24d1a9b642ddd2d51ac6f5752290
Tim Gates <tim.gates@iress.com>
2020-09-08 07:58:40 +1000

    docs: Fix simple typo, perentage -> percentage
    There is a small typo in gulpfile.js.
    
    Should read `percentage` rather than `perentage`.

--- gulpfile.js
+++ gulpfile.js
@@ -169,7 +169,7 @@ function bytediffFormatter(data) {
  * Format a number as a percentage
  * @param  {Number} num       Number to format as a percent
  * @param  {Number} precision Precision of the decimal
- * @return {Number}           Formatted perentage
+ * @return {Number}           Formatted percentage
  */
 function formatPercent(num, precision) {
     return (num * 100).toFixed(precision);
